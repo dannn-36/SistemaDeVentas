efaa336fceeceb97f2fa475bd2390f2cc07dc95f
Daniel <josdanvidu@gmail.com>
2025-05-11 17:53:15 -0500

    Product added

--- SistemaDeVentas/src/database_connection.py
+++ SistemaDeVentas/src/database_connection.py
@@ -23,6 +23,8 @@ class DatabaseConnection:
             print("\n--- Menú Principal ---")
             print("1. CRUD de Clientes")
             print("2. CRUD de Proovedores")
+            print("3. CRUD de Distritos")
+            print("4. CRUD de productos")
             opcion = input("Seleccione una opción: ")
 
             if opcion == "1":
@@ -31,6 +33,14 @@ class DatabaseConnection:
             elif opcion == "2":
                 from provider_crud import ProveedorCRUD
                 ProveedorCRUD.menu_crud_proveedores()
+                break
+            elif opcion == "3":
+                from district_crud import DistritoCRUD
+                DistritoCRUD.menu_crud_distritos()
+                break
+            elif opcion == "4":
+                from product_crud import ProductoCRUD
+                ProductoCRUD.menu_crud_productos()
                 break
             else:
                 print("Opción inválida. Intente nuevamente.")
