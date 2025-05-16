b619a5d1d725373ffec4f1efaf1e33e480f83c5e
Daniel <josdanvidu@gmail.com>
2025-05-15 22:02:33 -0500

    epico

--- SistemaDeVentas/src/main.py
+++ SistemaDeVentas/src/main.py
@@ -68,3 +68,9 @@ def main(page: ft.Page):
 if __name__ == "__main__":
     ft.app(target=main)
 
+
+#Proyecto final de la materia de Bases de Datos II
+# Integrantes:
+# - Daniel Villamil
+# - Sebastian Pineda
+# - Gianfranco Peniche
